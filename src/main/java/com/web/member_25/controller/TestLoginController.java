c24743ddb1cacdc78eb78be2d15e9ee94b8cd6f0
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2021-10-28 16:48:56 +0800

    debug 2021/10/28 04:48

--- src/main/java/com/web/member_25/controller/TestLoginController.java
+++ src/main/java/com/web/member_25/controller/TestLoginController.java
@@ -54,27 +54,27 @@ public class TestLoginController {
 
 //	@GetMapping("/zxczc")   //改
 //	public String indexDefault() {
-//		// System.out.println("回首頁");
+//		System.out.println("回首頁");
 //		return "member_25/tryIndex";
 //	}
-	
+
 	@GetMapping("/try/index")
 	public String index(@ModelAttribute("loginSession") membershipInformationBean mb, Model model) {
-		// System.out.println("login後回首頁GetMapping");
+		System.out.println("login後回首頁GetMapping");
 		membershipInformationBean mb2 = new membershipInformationBean();
 		mb2.setUserEmail(mb.getUserEmail());
 		mb2.setUserPwd(mb.getUserPwd());
 		mb2.setUserName(mb.getUserName());
 		model.addAttribute("loginSession", mb2);
-		// System.out.println("首頁的getmapping ----------->getUserEmail =" + mb.getUserName());
+		System.out.println("首頁的getmapping ----------->getUserEmail =" + mb.getUserName());
 
 		return "cart_30/TotalHome";
 	}
 
 	@PostMapping("/try/index")
 	public String processIndex(@ModelAttribute("loginSession") membershipInformationBean mb, Model model) {
-		// System.out.println("有進到POST Index哦-------->UserEmail " + mb.getUserEmail());
-		// System.out.println("有進到POST Index哦-------->");
+		System.out.println("有進到POST Index哦-------->UserEmail " + mb.getUserEmail());
+		System.out.println("有進到POST Index哦-------->");
 
 		return "cart_30/TotalHome";
 	}
@@ -98,42 +98,41 @@ public class TestLoginController {
 			Model model) {
 		
 		new MemberValidator().validate(mb, result);
-		// System.out.println("修改會員(PUT, 11-05): " + mb);
+		System.out.println("修改會員(PUT, 11-05): " + mb);
 		if (result.hasErrors()) {
-			// System.out.println("======================");
+			System.out.println("======================");
 			List<ObjectError> list = result.getAllErrors();
 			for (ObjectError error : list) {
-				// System.out.println("有錯誤：" + error);
+				System.out.println("有錯誤：" + error);
 			}
+			System.out.println("======================");
 			return "member_25/signUpPage";
-
-
 		}else {
-			// System.out.println("無錯誤-----帳號-------->"+mb.getUserEmail());
-			// System.out.println("無錯誤------密碼------->"+mb.getUserPwd());
+			System.out.println("無錯誤-----帳號-------->"+mb.getUserEmail());
+			System.out.println("無錯誤------密碼------->"+mb.getUserPwd());
 	
 		}
 		
 		
 
-		// System.out.println("-------------表單檢查無錯誤-------------");
-		// System.out.println("-------> mb = " + mb);
-		// System.out.println("---->mb.ID=" + mb.getId());
-		// System.out.println("---->mb.ID=" + mb.getUserEmail());
-		// System.out.println("---->mb.getIdentification=" + mb.getIdentification());
+		System.out.println("-------------表單檢查無錯誤-------------");
+		System.out.println("-------> mb = " + mb);
+		System.out.println("---->mb.ID=" + mb.getId());
+		System.out.println("---->mb.ID=" + mb.getUserEmail());
+		System.out.println("---->mb.getIdentification=" + mb.getIdentification());
 
 		Boolean loginResult=false;
 		
 		loginResult = memberService.overlappedAccount(mb.getUserEmail());
-		// System.out.println("----done------------------------=");
+		System.out.println("----done------------------------=");
 		if (loginResult ==false) {
-			// System.out.println("帳號重複  ----->");
+			System.out.println("帳號重複  ----->");
 
 			return "member_25/signUpPage_error";
 
 		} else if (loginResult == true) {
 
-			// System.out.println("無人使用此帳號 已註冊  ----->");
+			System.out.println("無人使用此帳號 已註冊  ----->");
 			mb.setIdentification("member");
 			
 			
@@ -166,7 +165,7 @@ public class TestLoginController {
 					StringUtils.arrayToCommaDelimitedString(suppressedFields));
 		}
 
-		// System.out.println("==========進入processMemberLogin=====================");
+		System.out.println("==========進入processMemberLogin=====================");
 		membershipInformationBean mb2 = new membershipInformationBean();
 
 		int loginResult = 0; 
@@ -174,7 +173,8 @@ public class TestLoginController {
 		String userEmail = null, userPwd = null;
 		userEmail = mb.getUserEmail();
 		userPwd = mb.getUserPwd();
-
+		System.out.println("登入表單的userEmail ======>" + userEmail);
+	
 		//登入結果   // 0錯誤 1成功 2無帳號 3重複帳號(除了自己+的應該不會出現這個可能)
 		loginResult = memberService.login(userEmail, userPwd);
 	
@@ -182,15 +182,14 @@ public class TestLoginController {
 
 			Boolean isMamber = true;
 			isMamber = memberService.memberOrManager(userEmail); // 判斷是使用者還是管理者
-			// System.out.println("isMamber------------>>管理者?----->" + isMamber);
+			System.out.println("isMamber------------>>管理者?----->" + isMamber);
 			if (isMamber == false) {
 				mb2.setUserEmail(userEmail);
 				mb2.setUserPwd(userPwd);
 				model.addAttribute("managerSession", mb2);
 				return "redirect:/manager_Ui";
 			}
-			// System.out.println("登入成功 userEmail  ----->" + userEmail);
-			mb2.setUserName("廖總");
+			System.out.println("登入成功 userEmail  ----->" + userEmail);
 			mb2.setUserEmail(userEmail);
 			mb2.setUserPwd(userPwd);
 			model.addAttribute("loginSession", mb2);
@@ -201,15 +200,11 @@ public class TestLoginController {
 			return "redirect:/"; // 登入成功
 
 		} else if (loginResult == 2) {
-
 			System.out.println("查無帳號  ----->");
 			return "member_25/loginpage_error";
-
 		} else if (loginResult == 3) {
-
 			System.out.println("帳號重複  ----->");
 			return "member_25/loginpage_error";
-
 		}
 
 		return "member_25/loginpage";
@@ -221,9 +216,9 @@ public class TestLoginController {
 		session.removeAttribute("loginSession");
 		session.removeAttribute("memberUiDefault");
 		session.removeAttribute("managerSession");
-		// System.out.println("logout:" + session.getAttribute("loginSession"));
+		System.out.println("logout:" + session.getAttribute("loginSession"));
 		sessionStatus.setComplete();
-		// System.out.println("已清除 登入狀態loginSession+managerSession+memberUiDefault");
+		System.out.println("已清除 登入狀態loginSession+managerSession+memberUiDefault");
 		return "redirect:/"; // 回乾淨首頁成功 讚
 	}
 
@@ -232,26 +227,26 @@ public class TestLoginController {
 	public String tologout2(SessionStatus sessionStatus) {
 
 		sessionStatus.setComplete(); // 讓本controller的sessionAtribute中的物件全部失效(也就是其他sessionAtribute創出來的物件不會刪到)
-		// System.out.println("已清除 所有登入狀態");
+		System.out.println("已清除 所有登入狀態");
 		return "redirect:/"; // 回乾淨首頁成功 讚
 	}
 
 	//登入後會員介面
 	@GetMapping("/try/member_Ui")
 	public String tryMemberUpdate(@ModelAttribute("loginSession") membershipInformationBean mb, Model model) {
-		// System.out.println("membershipInformationBean --getUserEmail----->" + mb.getUserEmail());
+		System.out.println("membershipInformationBean --getUserEmail----->" + mb.getUserEmail());
 		membershipInformationBean mb2 = new membershipInformationBean();
 		String userEmail = mb.getUserEmail();		
 		//預載會員資料
 		mb2=memberService.findMemberData(mb.getUserEmail());
 		if (mb2.getFileName()==null||mb2.getFileName().equals("")) {
-			// System.out.println("------登入後預載預設圖片------");
+			System.out.println("------登入後預載預設圖片------");
 		}
 		
 		
 
-		// System.out.println("getUserEmail --getMemberData2--><--->" + mb2.getUserEmail());
-		// System.out.println("getUserPwd --getMemberData2--><--->" + mb2.getUserPwd());
+		System.out.println("getUserEmail --getMemberData2--><--->" + mb2.getUserEmail());
+		System.out.println("getUserPwd --getMemberData2--><--->" + mb2.getUserPwd());
 		model.addAttribute("memberUiDefault", mb2);
 		
 		return "member_25/member_Ui";
@@ -270,12 +265,12 @@ public class TestLoginController {
 			// 把陣列裡面的元素用,隔開並轉成字串
 					StringUtils.arrayToCommaDelimitedString(suppressedFields));
 		}
-		// System.out.println("==========進入tryProcessMemberUpdate=====================");
+		System.out.println("==========進入tryProcessMemberUpdate=====================");
 		membershipInformationBean mb2 = new membershipInformationBean();
 		//先查DB的會員資料再去把表單的資料塞過去
 		mb2=memberService.findMemberDataAll(mb.getUserEmail());
-		// System.out.println("mb.getUserGender()+++++"+mb.getUserGender());
-		// System.out.println("mb.getUserName()+++++"+mb.getUserName());
+		System.out.println("mb.getUserGender()+++++"+mb.getUserGender());
+		System.out.println("mb.getUserName()+++++"+mb.getUserName());
 		mb2.setUserEmail(mb.getUserEmail());
 		mb2.setUserPwd(mb.getUserPwd());
 		mb2.setAddress(mb.getAddress());
@@ -285,36 +280,32 @@ public class TestLoginController {
 		mb2.setUserNickname(mb.getUserNickname());
 		mb2.setBirthday(mb.getBirthday());
 		mb2.setIdentification(mb.getIdentification());
-		// System.out.println("setIdentification---------->"+mb2.getIdentification());
+		System.out.println("setIdentification---------->"+mb2.getIdentification());
 	
 		
 		//下面是個處理好的bean 待update
 		mb2= new ProcessImage().uploadImage(mb2,mb);
-		// System.out.println("setIdentification2---------->"+mb2.getIdentification());
-		// System.out.println("-------------開始存入");
+		System.out.println("setIdentification2---------->"+mb2.getIdentification());
+		System.out.println("-------------開始存入");
 		memberService.save(mb2);
-		// System.out.println("-------------存入完畢");
+		System.out.println("-------------存入完畢");
 		
 	model.addAttribute("memberData", mb2);	
-
 		System.out.println("update success");
 		return "member_25/member_Ui";
-
 	}
 
 	@GetMapping("/try/delete")
 	public String tryMemberDelete(@ModelAttribute("loginSession") membershipInformationBean mb, HttpSession session,
 			HttpServletRequest request, HttpServletResponse response, SessionStatus sessionStatus, Model model) {
-		// System.out.println("membershipInformationBean --tryMemberDelete----->");
+		System.out.println("membershipInformationBean --tryMemberDelete----->");
 
 		memberService.deleteByName(mb.getUserEmail());
-		// System.out.println("------ 刪除完畢 ------");
+		System.out.println("------ 刪除完畢 ------");
 		tologout(session, request, response, sessionStatus);
-
 		System.out.println("------ 刪除session完畢 ------重新導向中...");
 			System.out.println("de---------------------------");
 		return "member_25/alertPage_delete";
-
 	}
 	
 	// 買家進化'
@@ -325,24 +316,23 @@ public class TestLoginController {
 		
 		//sellerData初始化設定
 //		model.addAttribute("sellerData", sellerDataMb);
-		// System.out.println("------sellerData 驗證後資料 name----------------?"+sellerDataMb.getUserName());
+		System.out.println("------sellerData 驗證後資料 name----------------?"+sellerDataMb.getUserName());
 		
 		membershipInformationBean mb2 = new membershipInformationBean();
 		
 		mb2 = memberService.findMemberDataAll(mb.getUserEmail());
 		model.addAttribute("sellerData", mb2);
 		
-		// System.out.println("---------------判斷會員資格中-----------------");
+		System.out.println("---------------判斷會員資格中-----------------");
 		if(!mb2.getIdentification().equals("seller")) {
 
 			System.out.println("---------------未認證會員-----------------");
 //			model.addAttribute("beanForVerificationCode",new membershipInformationBean());
 			model.addAttribute("memberUiDefault",new membershipInformationBean());
-
 			//包含驗證
 			return "member_25/seller/member_Ui_seller_default";
 		}
-		// System.out.println("---------準備進入已認證賣家中心---------");
+		System.out.println("---------準備進入已認證賣家中心---------");
 //		return "member_25/seller/member_Ui_seller";
 		return "redirect:/member/seller_Ui_v";
 	}
@@ -354,18 +344,17 @@ public class TestLoginController {
 			Model model) {
 		membershipInformationBean mb2 = new membershipInformationBean();			
 	mb=memberService.findMemberDataAll(mb.getUserEmail());
-
+	
 	System.out.println("---------預載已認證賣家中心---------");
-
 		return "member_25/seller/member_Ui_seller";		
 	}
 	//已認證賣家UI - 更新基本資料
 	@PostMapping("/member/seller_Ui_v")
 	public String processBuyerEvolution(@ModelAttribute("sellerData") membershipInformationBean mb, Model model) {
 		membershipInformationBean mb2 = new membershipInformationBean();
-		// System.out.println("---------已認證賣家中心更新資料---------");
+		System.out.println("---------已認證賣家中心更新資料---------");
 		memberService.save(mb);
-		// System.out.println("---------已認證賣家中心更新資料完畢---------");
+		System.out.println("---------已認證賣家中心更新資料完畢---------");
 		model.addAttribute("sellerData", mb);
 		return "member_25/seller/member_Ui_seller";
 	}
@@ -383,13 +372,11 @@ public class TestLoginController {
 				Model model) {
 			
 			membershipInformationBean mb2 = new membershipInformationBean();
-
 			mb2=memberService.findMemberDataAll(loginMb.getUserEmail());
 	
 			try {
 				System.out.println("------進入驗證馬環節-------");
 				int vcode=vcBean.getVerificationCode();
-
 				
 				
 				if (vcode==0 || Integer.valueOf(vcode)==null ) {
@@ -456,7 +443,7 @@ public class TestLoginController {
 //	// 管理者專用區
 //	@GetMapping("/manager_Ui")
 //	public String manager(@ModelAttribute("managerSession") membershipInformationBean mb, Model model) {
-//		// System.out.println("managerSession --manager----->" + mb.getUserEmail());
+//		System.out.println("managerSession --manager----->" + mb.getUserEmail());
 //
 //		return "manager_Ui";
 //	}
@@ -549,15 +536,15 @@ public class TestLoginController {
 				+ "\r\n"
 				+ "本網站隱私權保護政策將因應需求隨時進行修正，修正後的條款將刊登於網站上。");	  
 		mailSender.send(message); 
-		// System.out.println("------------------已寄出------------------ --->code="+VerificationCode);
-		// System.out.println("------------------已寄出------------------ --->code="+VerificationCode);
-		// System.out.println("------------------已寄出------------------ --->code="+VerificationCode);
+		System.out.println("------------------已寄出------------------ --->code="+VerificationCode);
+		System.out.println("------------------已寄出------------------ --->code="+VerificationCode);
+		System.out.println("------------------已寄出------------------ --->code="+VerificationCode);
 
-		// System.out.println("------------------loginSession------------------ --->code="+mb2.getUserEmail());
-		// System.out.println("------------------selletData------------------ --->code="+mb.getUserEmail());
+		System.out.println("------------------loginSession------------------ --->code="+mb2.getUserEmail());
+		System.out.println("------------------selletData------------------ --->code="+mb.getUserEmail());
 		vtBtn.setVerificationCode(VerificationCode);
 		model.addAttribute("beanForVerificationCode",vtBtn);
-
+	
 		return "redirect:/member/evolution";
 	}
 	
@@ -592,7 +579,6 @@ public class TestLoginController {
 		}
 			
 		return "member_25/manager/manager_List";
-
 	}
 	
 	
